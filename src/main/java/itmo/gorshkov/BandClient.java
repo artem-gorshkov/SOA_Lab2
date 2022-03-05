a8c158bba1e2bced7346f9ae809179154a6cd00a
a.v.gorshkov <a.v.gorshkov@tinkoff.ru>
2022-03-05 14:11:24 +0300

    adada

--- src/main/java/itmo/gorshkov/BandClient.java
+++ src/main/java/itmo/gorshkov/BandClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
-@FeignClient(value = "bandClient", url = "${service.host}", path = "/v1/api/band")
+@FeignClient(value = "bandClient", url = "${service.host}", path = "/v1/api/band", configuration = FeignConfig.class)
 public interface BandClient {
     @GetMapping("/{id}")
     MusicBand getBand(@PathVariable("id") Integer id);
---
+++ src/main/java/itmo/gorshkov/FeignConfig.java
@@ -0,0 +1,12 @@
+package itmo.gorshkov;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+
+public class FeignConfig {
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+}
